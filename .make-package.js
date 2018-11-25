9f387773d443f8c9a1ca30ecf0722e32351a66b8
abcb2 <kouji.wtnb@gmail.com>
2018-11-25 11:15:08 +0900

    add tsconfig

--- .make-package.js
+++ .make-package.js
@@ -4,7 +4,28 @@ let pkg = require('./package');
 let fs = require('fs-extra');
 
 const ROOT = 'dist/';
+const CJS_ROOT = ROOT + 'cjs/';
+const ESM5_ROOT = ROOT + 'esm5/';
+const ESM2015_ROOT = ROOT + 'esm2015/';
+const TYPE_ROOT = ROOT + 'typings/';
 const PKG_ROOT = ROOT + 'package/';
+const CJS_PKG = PKG_ROOT + '';
+const ESM5_PKG = PKG_ROOT + '_esm5/';
+const ESM2015_PKG = PKG_ROOT + '_esm2015/';
+const SRC_ROOT_PKG = PKG_ROOT + 'src/';
+const TYPE_PKG = PKG_ROOT;
 
 delete pkg.scripts;
 fs.removeSync(PKG_ROOT);
+
+let rootPackageJson = Object.assign({}, pkg, {
+  name: 'type-gimei',
+  main: './index.js',
+  typings: './index.d.ts',
+  module: './_esm5/index.js',
+  es2015: './esm2015/index.js',
+});
+
+console.log(pkg);
+console.log(rootPackageJson);
+
